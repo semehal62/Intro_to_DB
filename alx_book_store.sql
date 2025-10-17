05d8c042de25e0866ebbae25cf338f3fd69f294f
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-17 13:35:52 +0300

    Fix primary key syntax in SQL table definitions

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,30 +1,30 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 create table Authors(
-		author_id INT Primary Key,
+		author_id INT PRIMARY KEY,
 		author_name VARCHAR(215));
         
 CREATE TABLE Books(
-		book_id INT Primary Key,
+		book_id INT PRIMARY KEY,
 		title VARCHAR(130),
 		author_id int, 
 		price DOUBLE,
 		publication_date DATE);
 
 CREATE TABLE Customers(
-		customer_id INT Primary Key,
+		customer_id INT PRIMARY KEY,
 		customer_name VARCHAR(215),
 		email VARCHAR(215),
 		address TEXT);
 
 CREATE TABLE Orders(
-		order_id INT Primary Key,
+		order_id INT PRIMARY KEY,
 		customer_id INT,
 		order_date DATE
 );
 
 CREATE TABLE Order_Details(
-	orderdetailid INT Primary Key,
+	orderdetailid INT PRIMARY KEY,
 	order_id INT,
 	book_id INT,
 	quantity DOUBLE	
