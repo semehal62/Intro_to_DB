93b59cf36107b2f1029e27f0a1d1cd6589a187d4
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-17 13:42:28 +0300

    Fix foreign key references in SQL schema

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
-create table Authors(
+CREATE TABLE Authors(
 		author_id INT PRIMARY KEY,
 		author_name VARCHAR(215));
         
 CREATE TABLE Books(
 		book_id INT PRIMARY KEY,
 		title VARCHAR(130),
-		author_id int, 
+		FOREIGN KEY (author_id) REFERENCES Authors(authors_id),
 		price DOUBLE,
 		publication_date DATE);
 
@@ -19,13 +19,13 @@ CREATE TABLE Customers(
 
 CREATE TABLE Orders(
 		order_id INT PRIMARY KEY,
-		customer_id INT,
+		FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 		order_date DATE
 );
 
 CREATE TABLE Order_Details(
 	orderdetailid INT PRIMARY KEY,
-	order_id INT,
-	book_id INT,
+	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+	FOREIGN KEY (book_id) REFERENCES Books(book_id),
 	quantity DOUBLE	
 );
