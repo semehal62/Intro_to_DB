bbc1df45de43ee4f1207dabd6f782d2a32d84f70
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-18 23:00:40 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,6 @@
-
-    SELECT books
-    FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_SCHEMA = alx_book_store;
+-- task_4.sql
+-- Display the column names and types of the Books table
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
