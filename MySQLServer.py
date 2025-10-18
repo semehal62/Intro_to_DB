8328eb2df1d290abebcef9eab53fd1b42d82837b
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-18 22:16:46 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,14 +1,17 @@
 import mysql.connector
 
+
+mycursor = mydb.cursor()
+mycursor.excute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 # Replace with your connection details
 mydb = mysql.connector.connect(
     host="localhost",
     user="yourusername",
     password="yourpassword",
-    database="yourdatabase"
+    
 )
 
-mycursor = mydb.cursor()
+
 # Execute SQL statements using the execute() method on the cursor
 
 # Close connection to the databasse  
