480bf808bfd4009f7f88ce9d406037d81ad026f6
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-18 22:34:06 +0300

    Correct exception handling in MySQL database creation
    Fix syntax error in exception handling for database creation.

--- MySQLServer.py
+++ MySQLServer.py
@@ -6,6 +6,8 @@ try:
 except:
      if err.errno == 1007:  # Error code for "Can't create database; database exists"
             print(f"Database '{database_name}' already exists.")
+except mysql.connector.Error as e
+    print(e)
 else:
     print("Database 'alx_book_store' created successfully! ")
     mydb = mysql.connector.connect(
