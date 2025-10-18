ee48196d3943d0a2b9c5034c440afb3412acd30c
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-18 22:54:33 +0300

    Insert multiple customers into customers table
    Fixed syntax for multiple insert statements.

---
+++ task_6.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers (customer_id, customer_name, email, address )
+  VALUES (2, Blessing Malik, bmalik@sandtech.com, 124 Happiness  Ave.)
+         (3, Obed Ehoneah, eobed@sandtech.com, 125 Happines Ave.)
+         (4, Nehemial Kamolu, nkamolu@sandtech.com,126 Happines Ave.);
